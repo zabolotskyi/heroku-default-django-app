6f0e16e33f06a629b28ad3e5e7663d9ee7145afa
Roman Zabolotskyi <roman.zabolotskyi@gmail.com>
2021-06-03 23:16:49 +0300

    [HEROKU-1] test automatic review app

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ def index(request):
     times = int(os.environ.get('TIMES', 3))
     # r = requests.get('http://httpbin.org/status/418')
     # print(r.text)
-    return HttpResponse('Hello from DEV! ' * times)
+    return HttpResponse('Hello from PR on branch HEROKU-1! ' * times)
 
 
 
